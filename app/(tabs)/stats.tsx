42287a5be19999530c1a915bbaa5f9ec1cbeb92d
vedodeniro <181822760+vedodeniro@users.noreply.github.com>
2025-12-16 09:36:25 +0100

    added stats tab, lifetime cookies,Zustand shared state with functions and a reset button on stats page

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -1,31 +1,71 @@
-import { StyleSheet } from 'react-native';
+import { Text } from '@/components/Themed';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Link } from 'expo-router';
+import React, { useEffect } from 'react';
+import { useScoreStore } from '../store/useScoreStore';
+import { Pressable, StyleSheet, View } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
-}
+const { lifetimeScore, reset, load } = useScoreStore();
 
+  useEffect(() => {
+      load();
+    }, []);
+
+    return(
+
+      <LinearGradient
+              colors={['#f4a261', '#7c6015ff']}
+              style={styles.container}>
+
+          <View style={{ position: 'absolute', top: 1, right: 10 }}>
+            <Link href="/" asChild>
+                <Pressable style={styles.statsButton}>
+                  <Text style={styles.statsButtonText}>Cookie Clicker</Text>
+                </Pressable>
+              </Link>
+          </View>
+
+          <View style={{ position: 'absolute', right: 10, bottom: 10 }}>
+            <Pressable style={styles.statsButton} onPress={reset}>
+              <Text style={styles.statsButtonText}>Reset Stats</Text>
+            </Pressable>
+          </View>
+ 
+        <Text style={styles.statsText}>Lifetime Cookies: {lifetimeScore}</Text>
+
+      </LinearGradient>
+
+    );
+}
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
   },
-  title: {
-    fontSize: 20,
+
+  statsText: {
+    fontSize: 22,
+    color: '#ffffffff',
+  },
+  statsButton: {
+    backgroundColor: '#db9d6aff', 
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 20, 
+    marginTop: 20, 
+    alignItems: 'center', 
+
+  },
+
+  statsButtonText: {
+    color: 'white',
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    fontSize: 15,
+    
+  }
 });
